324aabc33e597344722892f92e0b44e9c8dad68a
Daniel Xu <hi@danielx.me>
2026-04-13 21:14:24 -0700

    add blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,27 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
+import {
+  transformerNotationDiff,
+  transformerNotationHighlight,
+  transformerNotationFocus,
+} from '@shikijs/transformers';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: 'vesper',
+      transformers: [
+        transformerNotationDiff(),
+        transformerNotationHighlight(),
+        transformerNotationFocus(),
+      ],
+    },
+  },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
