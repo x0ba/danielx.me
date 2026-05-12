6401c65e857a177d06406c0319319903d6eb66a1
Daniel Xu <hi@danielx.me>
2026-05-12 12:32:43 -0700

    Add local photo assets to homepage and blog post
    - Move featured images into imported Astro assets
    - Add the May 12, 2026 blog entry
    - Update content config to use astro/zod

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,9 @@
-import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
 const blog = defineCollection({
-  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blog' }),
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
   schema: ({ image }) =>
     z.object({
       title: z.string(),
